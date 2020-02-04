9c9f3d749de6659fc6ae768c998ac037a10276db
William Loh <william.loh@wustl.edu>
2020-02-04 10:35:18 -0600

    Update ClassExample.R

--- ClassExample.R
+++ ClassExample.R
@@ -6,17 +6,7 @@ print("Hello World")
 print("A new line")
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("A third line")
 
 
-
-abc
-=======
-print("A third line")
->>>>>>> ecd63aa3792f3b7d35d8b9e17da3a32bd6214edf
-=======
-print("A third line")
-print ("Alex Newman edit")
->>>>>>> 9a4d3ff74915d815dc086d14dd39256290a0f496
+print("test")
